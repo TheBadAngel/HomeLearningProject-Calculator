49843731f9cecedc450a21d9f78cfc642632a5e0
TheBadAngel <elisabethelida@hotmail.com>
2023-10-24 13:21:03 +0200

    Worked on the menu

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Calculator\n");
+
+            Console.WriteLine("Choose type of calculation.\n");
+
+            Console.WriteLine("Add, minus, multiply & divide.");
+            Console.WriteLine("Area.");
+            Console.WriteLine("Volume. \n");
+
+            Console.WriteLine("KPI");
+            Console.WriteLine("");
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
